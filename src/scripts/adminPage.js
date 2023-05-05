14919eedc8a115fdbb4833147835749074284c49
lauremiro <lauremiromelo@gmail.com>
2023-05-05 18:00:44 -0300

    estilizando pagens 1.3

---
+++ src/scripts/adminPage.js
@@ -0,0 +1,10 @@
+import { deletUser, createDepartament, delet, editDepartament, editUser, viewsDepartament } from "./modalAdmin.js";
+
+
+
+createDepartament ()
+viewsDepartament ()
+editDepartament ()
+delet ()
+editUser ()
+deletUser ()
